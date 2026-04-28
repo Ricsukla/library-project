fe4d003bb5c1e7b0546806e86706961c1cf5198c
Ricsukla <ricsukla@gmail.com>
2026-04-28 17:41:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ CORS(app)
 
 
 # ---------------- DATABASE ----------------
+
+DB_PATH = os.path.join(os.getcwd(), "library.db")
+
 def get_db():
-    conn = sqlite3.connect("library.db")
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
     conn.row_factory = sqlite3.Row
     return conn
 
